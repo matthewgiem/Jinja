5916083224b421ba74cbee14da24585b870eb123
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-11-03 15:30:14 +0700

    use datetime to print the year into our footer

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 import random
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    year = datetime.datetime.now().year
     random_number = random.randint(1, 10)
-    return render_template("index.html", num=random_number)
+    return render_template("index.html", num=random_number, footer=year)
 
 if __name__ == "__main__":
     app.run(debug=True)
