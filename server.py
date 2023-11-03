b0165b145c4bff2a40449c95db2be013951cd755
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-11-03 16:46:38 +0700

    use genderize and ageify api and integrate into my project

--- server.py
+++ server.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 from markupsafe import escape
 import random
 import datetime
+import requests
+import json
+
+AGEIFY_URL = "https://api.agify.io?name="
+GENDERIZE_URL = "https://api.genderize.io?name="
 
 app = Flask(__name__)
 
@@ -13,8 +18,14 @@ def home():
 
 @app.route('/guess/<name>')
 def guess(name):
+    age_json = requests.get(AGEIFY_URL + name)
+    gender_json = requests.get(GENDERIZE_URL + name)
+    age_result = json.loads(age_json.text)
+    gender_result = json.loads(gender_json.text)
+    age = age_result["age"]
+    gender = gender_result["gender"]
     year = datetime.datetime.now().year
-    return render_template("guess.html", footer=year, name=escape(name))
+    return render_template("guess.html", age=age, gender=gender, footer=year, name=escape(name))
 
 if __name__ == "__main__":
     app.run(debug=True)
