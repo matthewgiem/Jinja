826206418d9c277eb899816cc3d00dfd0a579482
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-11-06 17:37:14 +0700

    create a hyperlink to blog page using jinja

--- server.py
+++ server.py
@@ -28,7 +28,7 @@ def guess(name):
     return render_template("guess.html", age=age, gender=gender, footer=year, name=escape(name))
 
 @app.route("/blog")
-def blog():
+def get_blog():
     blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
     response = requests.get(blog_url)
     all_posts = json.loads(response.text)
